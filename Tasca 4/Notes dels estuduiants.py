f1a17557ccb3ca75ae2c5cd03fff6b27dab95beb
Pau López Millan <paulopez@iesmontsia.org>
2025-11-07 17:20:52 +0100

    Commit2

--- Tasca 4/Notes dels estuduiants.py
+++ Tasca 4/Notes dels estuduiants.py
@@ -1,12 +1,11 @@
-# Aquesta linia crea una llista amb les notes d'uns alumnes
-Notes = [1, 0, 6, 10, 8, 7, 5, -1, 9, 3, 2, 4]
-for Nota in Notes:
-    # Aquestes linies imprimiran les notes i comprovaran si hi ha algun 10
-    if Nota > -1:
-        print(Nota)
+# Aquesta linia crea una llista buida per guardar les notes
+Notes = []
+Nota = int(input("Introdueix una nota entre 0 i 10 (-1 per acabar): "))
+while Nota != -1:
+    Notes.append(Nota)
+    Nota = int(input("Introdueix una nota entre 0 i 10 (-1 per acabar): "))
     # Aquestes linies comproven si la nota es -1 per aturar el bucle
     if Nota == -1:
-        print(Nota)
         break
 # Aquestes linies comproven si hi ha hagut alguna nota que te un 10
 if max(Notes) == 10:
