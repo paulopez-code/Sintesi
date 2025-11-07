391d7879952947374bbd521f062ca6109c715a63
Pau López Millan <paulopez@iesmontsia.org>
2025-11-07 16:46:55 +0100

    commit

--- Positiu o negatiu en la llista (bucles).py
+++ Positiu o negatiu en la llista (bucles).py
@@ -3,6 +3,8 @@ Nobres = [-3, -4, 1, 5, 7, 6, -5, 4, 2, 3]
 # Aquestes linies imprimiran els nombres i comprovaran si hi ha algun nombre negatiu
 for Nombre in Nobres:
     print(Nombre)
+    if Nombre < 0:
+        break
 if min(Nobres) < 0:
     print("Hi ha nombres negatius")
 if min(Nobres) >= 0:
